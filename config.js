215444b03cc4b56cabbe626055a634c44d6c40e5
Pratt SAVI <58483678+PrattSAVI@users.noreply.github.com>
2022-03-01 10:20:50 -0500

    update gitnor

---
+++ config.js
@@ -0,0 +1,6 @@
+export var config = {
+    esri_key: "AAPK2e6f6235b63e4b53a08da4f04dc20b93BbuNAZ9o1yNKUN5Qgf2k8QykIDjHmdEyvvtYGXGoDcoWONg06lMrGiHkpdxKv5-Q",
+    clientId: "5GbDtJgZh2OQHlSF",
+    itemId: "6c5440b93bcd4caa9932778de15f768f",
+    fl_url: "https://services6.arcgis.com/IX0rbbPRMd0YfCJN/arcgis/rest/services/Map/FeatureServer",
+}
